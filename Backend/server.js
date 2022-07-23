55d86a1ff56bec6dff2f4c9e759fa2ee2d387bbb
rahulrajput83 <rahulrajput21112@gmail.com>
2022-07-23 17:10:10 +0530

    Updated Backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,15 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const ContactForm = require('./Data');
-const Projects = require('./Data')
+const Projects = require('./Data');
+const cors = require('cors')
 
 /* Initializes express app. */
 const app = express();
 
-const DEFAULT_PORT = 2400;
-
+const DEFAULT_PORT = 2800;
+app.use(cors());
+app.use(express.json());
 const server = app.listen(process.env.PORT || DEFAULT_PORT, function () {
     // Log a message to indicate that the server was started correctly
     const port = server.address().port;
@@ -19,26 +21,20 @@ mongoose.connect(process.env.connectionString)
     .then(() => {
         console.log('Mongoose Connected')
     })
+    .catch(() => {
+        console.log('Failed');
+    })
 
 
-/* Cors Header to allows data access to other domain. Frontend url */
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    next();
-});
-
-
-app.post('/', (req, res) => {
+app.post('/post', (req, res) => {
     const Contact1 = new ContactForm({
-        name: 'Rahul',
-        email: 'rahulrajput@gmail.com',
-        Subject: 'hello',
-        message: 'Hey from here'
+        name: req.body.name,
+        email: req.body.email,
+        Subject: req.body.subject,
+        message: req.body.message
     });
-    /* Save the model to mongoDB. */
     Contact1.save().then((data) => {
-        console.log('Data Saved...', data);
+        console.log(data);
     });
 });
 
