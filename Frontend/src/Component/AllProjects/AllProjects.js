10df96dfa66a0aee19babcc84a4eed4d6015d7dd
rahulrajput83 <rahulrajput21112@gmail.com>
2022-11-09 11:01:26 +0530

    Projects new design

--- Frontend/src/Component/AllProjects/AllProjects.js
+++ Frontend/src/Component/AllProjects/AllProjects.js
@@ -32,13 +32,13 @@ function AllProjects() {
             {
                 loading ? <Loading /> : null
             }
-            <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10 container mx-auto px-4 md:px-8 mt-4'>
+            <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10 container mx-auto px-4 md:px-8 mt-8'>
 
                 {
                     allprojects.map((data, index) => {
                         return (
                             <Link to={data._id} key={index} className='mx-auto group w-full bg-cover shadow-2xl pb-8 rounded-b-2xl transform duration-500 hover:-translate-y-4 cursor-pointer rounded-lg transition-all ease-in-out flex flex-col'>
-                                <img src={data.cover} alt='' className={`object-top group-hover:object-bottom hover:object-bottom p-1 h-72 transition-all duration-[2000ms] ease-in-out object-cover overflow-hidden w-full`} />
+                                <img src={data.cover} alt='' className={`object-top group-hover:object-bottom rounded-b-none rounded-lg hover:object-bottom h-72 transition-all duration-[2000ms] ease-in-out object-cover overflow-hidden w-full`} />
                                 <span className='text-center px-6 text-base font-medium mt-4 text-black'><ResponsiveEllipsis text={data.title} maxLine='1' ellipsis='...' trimRight basedOn='letters' /></span>
                                 <span className='text-justify px-6 mt-3 text-sm font-medium'><ResponsiveEllipsis text={data.description} maxLine='2' ellipsis='...' trimRight basedOn='letters' /></span>
                                 {/* <div className='mt-4 flex px-4 w-full justify-evenly'>
