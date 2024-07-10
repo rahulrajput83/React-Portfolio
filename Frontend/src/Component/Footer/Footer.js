01ad4986e9fae373342d8db064481cb72c4e86aa
Rahul Rajput <100850452+rahulrajput83@users.noreply.github.com>
2024-07-10 15:07:22 +0530

    Update Footer.js

--- Frontend/src/Component/Footer/Footer.js
+++ Frontend/src/Component/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='mb-4 flex flex-col w-full'>
         <span className='w-full mb-2 h-0.5 bg-gray-200'></span>
-        <div className='text-center text-sm mt-2 font-semibold'>Crafted with ❤️ <a href='https://github.com/rahulrajput83' className='text-second hover:underline'>Rahul Rajput</a></div>
+        <div className='text-center text-sm mt-2 font-semibold'>Crafted with ❤️ by <a href='https://github.com/rahulrajput83' className='text-second hover:underline'>Rahul Rajput</a></div>
     </div>
   )
 }
