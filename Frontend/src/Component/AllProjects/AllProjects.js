27a5b886b76562ceb99cc4f61de50d1389efdaeb
Rahul Rajput <rahul@techfino.in>
2024-10-17 19:50:50 +0530

    Changes

--- Frontend/src/Component/AllProjects/AllProjects.js
+++ Frontend/src/Component/AllProjects/AllProjects.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import Loading from './Loading';
 import LinesEllipsis from 'react-lines-ellipsis'
 import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { FaGithub } from 'react-icons/fa';
+import { MdRemoveRedEye } from 'react-icons/md';
 
 const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis)
 
@@ -38,20 +40,55 @@ function AllProjects() {
                 {
                     allprojects.map((data, index) => {
                         return (
-                            <Link to={`/project/${data._id}`} key={index} className='mx-auto group w-full bg-cover shadow-2xl pb-8 rounded-b-2xl transform duration-500 hover:-translate-y-4 cursor-pointer rounded-lg transition-all ease-in-out flex flex-col'>
-                                <img src={data.cover} alt='' className={`object-top group-hover:object-bottom rounded-b-none rounded-lg hover:object-bottom h-72 transition-all duration-[2000ms] ease-in-out object-cover overflow-hidden w-full`} />
-                                <span className='text-center px-6 text-base font-medium mt-4 text-black'><ResponsiveEllipsis text={data.title} maxLine='1' ellipsis='...' trimRight basedOn='letters' /></span>
-                                <span className='text-justify px-6 mt-3 text-sm font-medium'><ResponsiveEllipsis text={data.description} maxLine='2' ellipsis='...' trimRight basedOn='letters' /></span>
-                                {/* <div className='mt-4 flex px-4 w-full justify-evenly'>
-                                    <a href={data.codelink} className='px-6 flex items-center py-2 rounded-md bg-main hover:bg-second text-white font-medium'>
-                                        <FaGithub className='mr-2' />
-                                        <span>GitHub</span>
+                            <Link
+                                to={`/project/${data._id}`}
+                                key={index}
+                                className="mx-auto relative group w-full bg-cover shadow-2xl pb-8 rounded-b-2xl transform duration-700 hover:-translate-y-4 cursor-pointer rounded-lg transition-all ease-in-out flex flex-col"
+                            >
+                                <div className="absolute top-0 right-0 gap-3 pr-2 pt-2 flex flex-col -translate-x-full opacity-0 group-hover:opacity-100 group-hover:translate-x-0 transition-transform duration-700 ease-in-out z-0 group-hover:z-10">
+                                    <a
+                                        href={data.codelink}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        onClick={(e) => e.stopPropagation()}
+                                        className="text-second p-3 gap-1 hover:text-white bg-white shadow-2xl flex items-center justify-center transition-all ease-in-out duration-300 hover:bg-second rounded font-medium"
+                                    >
+                                        <FaGithub className="text-xl" />
                                     </a>
-                                    <a href={data.demolink} className='px-6 flex items-center py-2 rounded-md bg-main hover:bg-second text-white font-medium'>
-                                        <FaEye className='mr-2' />
-                                        <span>Demo</span>
+                                    <a
+                                        href={data.demolink}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        onClick={(e) => e.stopPropagation()}
+                                        className="text-white p-3 gap-1 hover:text-second flex items-center shadow-2xl justify-center transition-all ease-in-out duration-300 hover:bg-white bg-second rounded font-medium"
+                                    >
+                                        <MdRemoveRedEye className="text-xl" />
                                     </a>
-                                </div> */}
+                                </div>
+
+                                <img
+                                    src={data.cover}
+                                    alt=""
+                                    className={`object-top group-hover:object-bottom hover:object-bottom rounded-b-none h-72 transition-all duration-[2000ms] rounded-lg ease-in-out object-cover overflow-hidden w-full`}
+                                />
+                                <span className="text-center px-6 text-base font-medium mt-4 text-black">
+                                    <ResponsiveEllipsis
+                                        text={data.title}
+                                        maxLine="1"
+                                        ellipsis="..."
+                                        trimRight
+                                        basedOn="letters"
+                                    />
+                                </span>
+                                <span className="text-justify px-6 mt-3 text-sm font-medium">
+                                    <ResponsiveEllipsis
+                                        text={data.description}
+                                        maxLine="2"
+                                        ellipsis="..."
+                                        trimRight
+                                        basedOn="letters"
+                                    />
+                                </span>
                             </Link>
                         )
                     })
--- Frontend/src/Component/ProjectDetail/ProjectDetail.jsx
+++ Frontend/src/Component/ProjectDetail/ProjectDetail.jsx
@@ -1,11 +1,12 @@
 import React, { useCallback } from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { FaEye, FaGithub } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 import { useParams } from 'react-router-dom'
 import Loading from '../AllProjects/Loading'
 import avtar from '../Assets/mf-avatar.svg'
 import Input from './Input';
+import { MdRemoveRedEye } from "react-icons/md";
 
 function ProjectDetail() {
     const { id } = useParams();
@@ -103,7 +104,7 @@ function ProjectDetail() {
     }, [id, getFeedback])
 
 
-    return !loading ? <div className='w-full p-2 mt-2 sm:mt-0 sm:p-8 md:px-12 lg:px-20 flex flex-col'>
+    return !loading ? <div className='w-full mt-20 p-2 sm:p-8 md:px-12 lg:px-20 flex flex-col'>
         <div className='w-full flex flex-col-reverse sm:flex-row justify-between sm:items-center'>
             <span className='text-lg w-full mt-4 sm:mt-0 font-medium text-black'>{data.title}</span>
             <div className='flex w-full gap-4 justify-end'>
@@ -112,7 +113,7 @@ function ProjectDetail() {
                     <span className='font-medium text-sm'>GitHub</span>
                 </a>
                 <a href={data.demolink} target='_blank' rel='noreferrer' className='text-white p-2 gap-1 hover:text-main flex items-center justify-center group transition-all ease-in-out duration-300 hover:bg-white bg-second rounded border-[0.12rem] border-second font-medium'>
-                    <FaEye className='text-xl text-white group-hover:text-second' />
+                    <MdRemoveRedEye className='text-xl text-white group-hover:text-second' />
                     <span className='font-medium text-sm'>Live</span>
                 </a>
             </div>
