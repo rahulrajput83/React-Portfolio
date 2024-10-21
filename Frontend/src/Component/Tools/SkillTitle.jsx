ec578cd79b4d2cf68e62d167a0984a31cd04f918
Rahul Rajput <rahul@techfino.in>
2024-10-21 17:41:08 +0530

    Skill Updates

---
+++ Frontend/src/Component/Tools/SkillTitle.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+export default function Popover({id, title, src}) {
+  const [isPopoverVisible, setIsPopoverVisible] = useState(false);
+
+  return (
+    <div className="relative inline-block text-center">
+      <img
+        data-popover-target={id}
+        className="w-10 mx-2 cursor-pointer md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10"
+        alt={title}
+        src={src}
+        onMouseEnter={() => setIsPopoverVisible(true)}
+        onMouseLeave={() => setIsPopoverVisible(false)}
+      />
+
+      {isPopoverVisible && (
+        <div
+          role="tooltip"
+          className="absolute z-10 w-36 text-sm border border-gray-200 rounded-lg bg-second shadow-2xl"
+          style={{
+            bottom: '90%',
+            left: '50%',
+            transform: 'translateX(-50%)',
+          }}
+        >
+          
+          <div className="px-3 text-sm  py-2 text-white font-medium">
+            <span>{title}</span>
+          </div>
+          <div className="absolute bottom-[-6px] left-1/2 transform -translate-x-1/2 w-4 h-4 bg-second border-b border-r border-second rotate-45"></div>
+        </div>
+      )}
+    </div>
+  );
+}
--- Frontend/src/Component/Tools/Tools.js
+++ Frontend/src/Component/Tools/Tools.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import SkillTitle from './SkillTitle'
 
 function Tools() {
     return (
@@ -7,8 +8,29 @@ function Tools() {
                 <span>Languages and <span className='text-second'>Tools:</span></span>
             </h1>
             <div className='w-full justify-center items-center p-2 px-0 md:p-0 md:w-11/12 flex flex-wrap'>
-                <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='TypeScript' src="https://devicon-website.vercel.app/api/typescript/original.svg"></img>
-                <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='JavaScript' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+                {/* <img data-popover-target="popover-typescript" className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='TypeScript' src="https://devicon-website.vercel.app/api/typescript/original.svg"></img> */}
+                <SkillTitle id="popover-typescript" title="TypeScript" src="https://devicon-website.vercel.app/api/typescript/original.svg" />
+                <SkillTitle id="popover-javascript" title="JavaScript" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+                <SkillTitle id="popover-nextjs" title="NextJS" src="https://devicon-website.vercel.app/api/nextjs/original.svg" />
+                <SkillTitle id="popover-reactjs" title="ReactJS" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+                <SkillTitle id="popover-angular" title="Angular" src="https://devicon-website.vercel.app/api/angularjs/plain.svg" />
+                <SkillTitle id="popover-bootstrap" title="Bootstrap" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+                <SkillTitle id="popover-tailwind" title="Tailwind CSS" src="https://devicon-website.vercel.app/api/tailwindcss/original-wordmark.svg" />
+                <SkillTitle id="popover-sass" title="SASS" src="https://devicon-website.vercel.app/api/sass/original.svg" />
+                <SkillTitle id="popover-nodejs" title="NodeJS" src="https://devicon-website.vercel.app/api/nodejs/original.svg" />
+                <SkillTitle id="popover-expressjs" title="ExpressJS" src="https://devicon-website.vercel.app/api/express/original.svg" />
+                <SkillTitle id="popover-python" title="Python" src="https://devicon-website.vercel.app/api/python/original.svg" />
+                <SkillTitle id="popover-fastapi" title="FastAPI" src="https://devicon-website.vercel.app/api/fastapi/original.svg" />
+                <SkillTitle id="popover-mongo" title="MongoDB" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+                <SkillTitle id="popover-mysql" title="MySQL" src="https://devicon-website.vercel.app/api/mysql/original.svg" />
+                <SkillTitle id="popover-html" title="HTML" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+                <SkillTitle id="popover-css" title="CSS" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+                <SkillTitle id="popover-git" title="Git" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+                <SkillTitle id="popover-github" title="GitHub" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+                <SkillTitle id="popover-gitlab" title="GitLab" src="https://devicon-website.vercel.app/api/gitlab/original.svg" />
+                <SkillTitle id="popover-vscode" title="VS Code" src="https://devicon-website.vercel.app/api/vscode/original.svg" />
+
+                {/* <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='JavaScript' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
                 <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='NextJS' src="https://devicon-website.vercel.app/api/nextjs/original.svg"></img>
                 <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='ReactJS' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
                 <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='Angular' src="https://devicon-website.vercel.app/api/angularjs/plain.svg"></img>
@@ -26,7 +48,7 @@ function Tools() {
                 <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='Git' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
                 <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='GitHub' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
                 <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='GitLab' src="https://devicon-website.vercel.app/api/gitlab/original.svg"></img>
-                <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='VS Code' src="https://devicon-website.vercel.app/api/vscode/original.svg"></img>
+                <img className='w-10 mx-2 md:w-16 md:h-16 md:mx-10 md:my-5 my-4 h-10' alt='VS Code' src="https://devicon-website.vercel.app/api/vscode/original.svg"></img> */}
             </div>
         </div>
     )
